56e31257c87ccbde71058e6dc0b43b354e3975c8
Alex Fructo <alexfructo@gmail.com>
2024-02-21 23:35:28 -0300

    Incluído componente para criar e atualizar um customer através do fronte nd.

--- frontend/src/CustomersService.js
+++ frontend/src/CustomersService.js
@@ -1,39 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:8000';
+const API_URL = "http://localhost:8000";
 
-export default class CustomersService{
+export default class CustomersService {
+  constructor() {}
 
-    constructor(){}
+  getCustomers() {
+    const url = "${API_URL}/api/customers/";
+    return axios.get(url).then((response) => response.data);
+  }
 
-    getCustomers(){
+  getCustomersByURL(link) {
+    const url = "${API_URL}$link";
+    return axios.get(url).then((response) => response.data);
+  }
 
-        const url = '${API_URL}/api/customers/';
-        return axios.get(url).then(response => response.data);
-    }
+  getCustomer(pk) {
+    const url = "${API_URL}/api/customers/${pk}";
+    return axios.get(url).then((response) => response.data);
+  }
 
-    getCustomersByURL(link){
-        const url = '${API_URL}$link';
-        return axios.get(url).then(response => response.data);
-    }
-    
-    getCustomer(pk){
-        const url = '${API_URL}/api/customers/${pk}';
-        return axios.get(url).then(response => response.data);
-    }
+  deleteCustomer(customer) {
+    const url = "${API_URL}/api/customers/${customer.pk}";
+    return axios.delete(url);
+  }
 
-    deleteCustomer(customer){
-        const url = '${API_URL}/api/customers/${customer.pk}';
-        return axios.delete(url);
-    }
+  createCustomer(customer) {
+    const url = "${API_URL}/api/customers/";
+    return axios.post(url, customer);
+  }
 
-    createCustomer(customer){
-        const url = '${API_URL}/api/customers/';
-        return axios.post(url, customer);
-    }
-
-    updateCustomer(customer){
-        const url = '${API_URL}/api/customers/${customer.pk}';
-        return axios.put(url, customer);
-    }
+  updateCustomer(customer) {
+    const url = "${API_URL}/api/customers/${customer.pk}";
+    return axios.put(url, customer);
+  }
 }
